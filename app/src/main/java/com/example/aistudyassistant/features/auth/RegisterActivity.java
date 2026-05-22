a3c52b23970c105a7b852b3c12f276ffd0a20fe0
Eternal <et3rnxl24@gmail.com>
2026-05-22 23:58:31 +0700

    pushing

--- app/src/main/java/com/example/aistudyassistant/features/auth/RegisterActivity.java
+++ app/src/main/java/com/example/aistudyassistant/features/auth/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.aistudyassistant.features.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,6 @@ import com.example.aistudyassistant.R;
 import com.example.aistudyassistant.network.ApiClient;
 import com.example.aistudyassistant.network.models.RegisterRequest;
 import com.example.aistudyassistant.network.models.AuthResponse;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
